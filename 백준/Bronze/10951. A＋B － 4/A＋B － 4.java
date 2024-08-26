a1620886ae1cec1982090d4c07a1bb7eed49a50e
mike7643 <121170730+mike7643@users.noreply.github.com>
2024-08-26 13:25:51 +0900

    [Bronze V] Title: A+B - 4, Time: 184 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int a,b,total;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            total = a + b;
+            System.out.println(total);
+        }
+    }
+}
