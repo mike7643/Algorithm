7a4bf61b3523117eaa32121578f549462c0866f9
mike7643 <121170730+mike7643@users.noreply.github.com>
2025-02-23 10:08:07 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 26480 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,31 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {  // 최소 힙
+public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pqueue = new PriorityQueue<>();  // PQ 기본적으로 최소힙(오름차순 정렬 poll)
+        PriorityQueue<Integer> pqueue = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());  // x를 num 이라고 선언하고 인풋받기
-            if (num == 0) {     // 만약 x 가 0이면
-                if (pqueue.isEmpty()) { // PQ 비었으면
-                    bw.write('0');
-                    bw.newLine();
-                } else {
-                    bw.write(String.valueOf(pqueue.poll()));
-                    bw.newLine();
-                }
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sb.append(pqueue.isEmpty() ? "0" : pqueue.poll()).append("\n");
             } else {
                 pqueue.offer(num);
             }
         }
 
-        // 출력
-        bw.flush();
-        bw.close();
+        System.out.print(sb); // 한 번에 출력
         br.close();
     }
 }
