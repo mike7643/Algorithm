ae7a35e7664b92a1c73517ad740be26ab6b2951b
mike7643 <121170730+mike7643@users.noreply.github.com>
2025-02-23 10:05:06 +0900

    [Silver II] Title: 최소 힙, Time: 292 ms, Memory: 26724 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,28 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main {  // 최소 힙
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pqueue = new PriorityQueue<>();  // PQ 기본적으로 최소힙(오름차순 정렬 poll)
 
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(br.readLine());
-
-            //x가 자연수 일 때
-            if (x > 0) {
-                queue.offer(x);
-                continue;
+            int num = Integer.parseInt(br.readLine());  // x를 num 이라고 선언하고 인풋받기
+            if (num == 0) {     // 만약 x 가 0이면
+                if (pqueue.isEmpty()) { // PQ 비었으면
+                    bw.write('0');
+                    bw.newLine();
+                } else {
+                    bw.write(String.valueOf(pqueue.poll()));
+                    bw.newLine();
+                }
+            } else {
+                pqueue.offer(num);
             }
-
-            //x가 0 일때
-            //근데 만약 배열 비어있으면
-            if (queue.isEmpty()) System.out.println(0);
-            else System.out.println(queue.poll());
         }
+
+        // 출력
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
