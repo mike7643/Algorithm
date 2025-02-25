e879251bd3c3d49db79e04c67d835d3777dc80f4
mike7643 <121170730+mike7643@users.noreply.github.com>
2025-02-25 22:25:28 +0900

    [Silver IV] Title: 차집합, Time: 1636 ms, Memory: 212956 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        TreeSet<Integer> set1 = new TreeSet<>();
+        TreeSet<Integer> set2 = new TreeSet<>();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < a; i++) {
+            set1.add(Integer.parseInt(st.nextToken()));
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < b; i++) {
+            set2.add(Integer.parseInt(st.nextToken()));
+        }
+        // 집합 a에는 속하면서 집합 b에는 속하지 않는 원소의 개수
+        List<Integer> res = new ArrayList<>();
+        for (int num : set1) {
+            if (!set2.contains(num)) {
+                res.add(num);
+            }
+        }
+        // 구체적인 원소 오름차순 정렬 출력
+        Collections.sort(res);
+
+        StringBuilder sb = new StringBuilder();
+        if (res.isEmpty()) {
+            sb.append("0");
+        } else {
+            sb.append(res.size()).append("\n");
+            for (int num : res) {
+                sb.append(num).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
